884ced8415fdb71d24b3379bfce8a2451ddf3955
JammyCat91283 <marktaoun@gmail.com>
2025-04-13 12:48:06 -0500

    Update main.js

--- main.js
+++ main.js
@@ -1,8 +1,15 @@
 let room = new WebsimSocket();
 let user = room.peers[room.clientId];
-let name = user.username
-if (name!=="blueyellow"&&name!=="TheEthicalHacker") {
+let name = user.username;
+
+// Check for unauthorized users
+if (name !== "blueyellow" && name !== "TheEthicalHacker") {
+  // Destroy the image element with id 'targetImage'
+  const targetImage = document.getElementById('targetImage');
+  if (targetImage) {
+    targetImage.remove();
+  }
   return;
 }
 
-console.log("Loading cheats for Websim....")
+console.log("Loading cheats for Websim....");
