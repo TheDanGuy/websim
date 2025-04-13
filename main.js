047b8c03b0465c10229816a692b32c36b6896945
JammyCat91283 <marktaoun@gmail.com>
2025-04-13 12:44:04 -0500

    Create main.js

---
+++ main.js
@@ -0,0 +1,8 @@
+let room = new WebsimSocket();
+let user = room.peers[room.clientId];
+let name = user.username
+if (name!=="blueyellow"&&name!=="TheEthicalHacker") {
+  return;
+}
+
+console.log("Loading cheats for Websim....")
