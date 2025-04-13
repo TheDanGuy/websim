7503c07063a688e4a3bd737f527b34072f1d73e6
JammyCat91283 <marktaoun@gmail.com>
2025-04-13 12:57:28 -0500

    Update main.js

--- main.js
+++ main.js
@@ -3,13 +3,12 @@ let user = room.peers[room.clientId];
 let name = user.username;
 
 // Check for unauthorized users
-if (name !== "blueyellow" && name !== "TheEthicalHacker") {
+if (name == "blueyellow" || name == "TheEthicalHacker") {
+  console.log("Loading cheats for Websim....");
+} else {
   // Destroy the image element with id 'targetImage'
   const targetImage = document.getElementById('targetImage');
   if (targetImage) {
     targetImage.remove();
   }
-  return;
 }
-
-console.log("Loading cheats for Websim....");
