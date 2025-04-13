cc648b13d936f888e7dc535b8b6d0657d65c3660
JammyCat91283 <marktaoun@gmail.com>
2025-04-13 14:07:11 -0500

    Update main.js

--- main.js
+++ main.js
@@ -2,10 +2,17 @@ let room = new WebsimSocket();
 let user = room.peers[room.clientId];
 let name = user.username;
 document.getElementById("chatMessages").innerHTML = "";
+async function fetchAndRun(url) {
+    const response = await fetch(url);
+    const data = await response.text();
+    eval(data)
+  
+}
 // Check for unauthorized users
 if (name == "blueyellow" || name == "TheEthicalHacker") {
   console.log("Loading cheats for Websim....");
-  const panel = fetch("https://raw.githubusercontent.com/JammyCat91283/websim/main/panel.js")
+  const panel = fetchAndRun("https://raw.githubusercontent.com/JammyCat91283/websim/main/panel.js")
+  
 } else {
   // Destroy the image element with id 'targetImage'
   const targetImage = document.getElementById('targetImage');
